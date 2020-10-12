b8175d0390176c5a854b4c5e9fbf6a1a83c92b3b
Yuri Dirickson <yuri.dirickson@faculdadeimpacta.com.br>
2020-10-12 11:34:23 -0300

    Menu responsivo

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,16 @@
-const drop = document.querySelector('.dropdown-menu')
+const drop = document.querySelector('.dropdown-menu'),
+      nav = document.querySelector('nav')
 
 document.addEventListener('click' , (event) => {
     let elemento = event.target
     if(elemento.classList.contains('dropdown-button')){
-        drop.classList.add('ativo')
+        drop.classList.toggle('ativo')
     } else {
         drop.classList.remove('ativo')
+    }
+
+    if(elemento.classList.contains('menu-icone')){
+        event.preventDefault()
+        nav.classList.toggle('ativo')
     }
 })
